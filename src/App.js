250355036209423da6cf25bcf343de892c4510d1
Andre Giron <andre@beamlabs.net>
2019-08-21 18:34:41 -0400

    Adds navigation bar. Resolves #6.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import ECommerceNavigationBar from './components/Navbar/ECommerceNavigationBar';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="primary">Primary</Button>
-
+      <header>
+        <ECommerceNavigationBar />
       </header>
     </div>
   );
